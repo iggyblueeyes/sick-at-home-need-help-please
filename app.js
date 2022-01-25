5017aa14212396054c502b248744f5a058328ebb
calebjignace <cignace@asu.edu>
2022-01-24 22:59:22 -0700

    First commit.

---
+++ app.js
@@ -0,0 +1,26 @@
+function setNavButton(buttonID, htmlFileName) {
+    document.getElementById(buttonID).onclick = function() {
+	window.location = htmlFileName;
+    }
+}
+
+// Input filtering
+
+function removeLastKeyIfNonLetter(input, lastKey) {
+    if (!"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(lastKey)) {
+        input.value = input.value.slice(0, -1);
+    }
+}
+
+function removeLastKeyIfNonNumeric(input, lastKey) {
+    if (!"1234567890".includes(lastKey)) {
+        input.value = input.value.slice(0, -1);
+    }
+}
+
+function removeLastKeyIfTooLong(input, maxLength) {
+    if (input.value.length > maxLength) {
+	input.value = input.value.slice(0, -1);
+    }
+}
+
